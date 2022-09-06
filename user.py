349bf79bcc6f514a518e0e109cec6952b5676d04
Yuzhu Duan <yduan004@ucr.edu>
2022-09-05 19:34:10 -0700

    use sqlite3

--- user.py
+++ user.py
@@ -1,5 +1,85 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+import hmac
+from flask_jwt_extended import create_access_token
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+    
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        connection.close()
+        return user
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type = str,
+        required = True,
+        help = "'This field cannot be blank."
+    )
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help = "'This field cannot be blank."
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        if User.find_by_username(data['username']):
+            return {'message': 'A user with that username already exists'}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+        connection.commit()
+        connection.close()
+        return {'message': 'User created successfully.'}, 201
+
+class UserLogin(Resource):
+    user_parser = reqparse.RequestParser()
+    user_parser.add_argument(
+        'username',
+        type=str,
+        required=True,
+        help="This field cannot be blank.")
+    user_parser.add_argument(
+        'password',
+        type=str,
+        required=True,
+        help="This field cannot be blank.")
+
+    def post(self):
+        data = UserLogin.user_parser.parse_args()
+        user = User.find_by_username(data['username'])
+        if user and hmac.compare_digest(user.password, data['password']):
+            access_token = create_access_token(identity=user.id)
+            return {'access_token': access_token}, 200
+        return {'message': 'Invalid Credentials!'}, 401
