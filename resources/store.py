7141c145a56b28232753ad0d1ea78b13a4274ceb
Yuzhu Duan <yduan004@ucr.edu>
2022-09-05 23:52:02 -0700

    small edit

--- resources/store.py
+++ resources/store.py
@@ -10,7 +10,7 @@ class Store(Resource):
 
     def post(self, name):
         if StoreModel.find_by_name(name):
-            return {"message": "A store with name '{}' already exists.".format(mat)}, 400
+            return {"message": "A store with name '{}' already exists.".format(name)}, 400
         store = StoreModel(name)
         try:
             store.save_to_db()
